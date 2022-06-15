92bd1becc7a7d1cd1dc83f138df8b8c7df14e01b
Sophia Jia <xjia@presagia.ai>
2022-06-15 13:04:49 -0400

    add more tests

--- tests/testthat/test-entry_db.R
+++ tests/testthat/test-entry_db.R
@@ -19,9 +19,9 @@ test_that("make_doce_pub",{
   expect_equal(dim(doce_pub),c(1, 5))
 })
 
-test_that("combine outcomes of the same times",{
+test_that("extract_outcome - get columns of the outcome ",{
   tmp <- extract_outcome("os",combined_df[1,])
-  expect_equal(dim(tmp),c(1, 16))
+  expect_equal(dim(tmp),c(1, 18))
   
   tmp <- extract_outcome("con",combined_df[1,])
   expect_equal(dim(tmp),c(1, 9))
@@ -42,26 +42,59 @@ test_that("make_doce_surv_curve - take km from excel add to survival outcome obj
   expect_equal(class(t2), "NULL")
   expect_equal(t2, NULL)
   
-  
   t3 <- make_doce_surv_curve(combined_df_inc[1,]$`os-km_data`)
   expect_identical(t2, t3)
   
 })
 
 
+test_that("make_doce_surv_curve - take km from excel add to survival outcome object",{
+  t1 <- make_doce_surv_curve(combined_df[1,]$`os-km_data`)
+  
+  expect_equal(class(t1),c("survival_curve", "data.frame"))
+  expect_equal(dim(t1), c(106,2))
+  
+  expect_error(make_doce_surv_curve(combined_df[1,]$`os-km_data`[[1]]))
+  expect_error(make_doce_surv_curve(NA))
+  
+  t2 <- make_doce_surv_curve(vector(mode = "list", length = 1))
+  expect_equal(class(t2), "NULL")
+  expect_equal(t2, NULL)
+  
+  t3 <- make_doce_surv_curve(combined_df_inc[1,]$`os-km_data`)
+  expect_identical(t2, t3)
+  
+})
+
+test_that("make_doce_surv_fig - test save survival fig to database",{
+  
+  t1 <- make_doce_surv_fig(combined_df[1,]$`os-fig_path`)
+  expect_equal(class(t1),c("survival_figures", "tbl_df", "tbl" , "data.frame"))
+  
+  t2 <- make_doce_surv_fig(combined_df_inc[1,]$`os-fig_path`)
+  expect_equal(class(t2), "NULL")
+  expect_equal(t2, NULL)
+  
+})
+
+
+
 test_that("test survival outcome",{
   tmp <- make_surv_outcome("os",combined_df[1,],input_info)
    
   expect_true("survival_outcome" %in% class(tmp))
-  expect_equal(dim(tmp), c(1,19))
+  expect_equal(dim(tmp), c(1,20))
   expect_equal(class(tmp$survival_curve),"list")
   expect_equal(class(tmp$survival_curve[[1]]),c("survival_curve", "data.frame"))
+  expect_equal(class(tmp$survival_figures),"list")
+  expect_equal(class(tmp$survival_figures[[1]]),c("survival_figures", "tbl_df", "tbl" , "data.frame"))
   
   t2 <- make_surv_outcome("os",combined_df_inc[1,],input_info)
   expect_equal(dim(t2),dim(tmp))
   expect_equal(class(t2$survival_curve),"list")
-  expect_equal(class(t2$survival_curve[[1]]),c("survival_curve", "data.frame")) 
-
+  expect_equal(class(t2$survival_curve[[1]]),"NULL")
+  expect_equal(class(t2$survival_figures),"list")
+  expect_equal(class(t2$survival_figures[[1]]),"NULL")
 })
 
 test_that("test continous outcome",{
@@ -96,17 +129,32 @@ test_that("make_trial - take all of the info and make a single trial object for 
   expect_equal(typeof(single_trial),"list")
   expect_equal(length(single_trial),7)
   
-  ## when each of the outcome is not excist
-  
-  
-  # test special case of input_info
-  
-  
-  
-  # test special case of outcome_list 
-  
-  
-  # test every NULL case of the input. 
-  
 })
+# 
+test_that("write_trial - write tiral in to duckdb",{
+
+  ## perfect cases
+
+  outcome_list <- make_doce_outcome(combined_df[1,],input_info)
+  doce_pub <- make_doce_pub(input_info)
+
+  single_trial <- make_trial(input_info,outcome_list,doce_pub,"nct111")
+  write_trial(single_trial, trial_con_db)
+
+})
+
+# 
+# test_that("write_trial - write tiral in to duckdb (abnormal cases)",{
+#   
+#   ## perfect cases
+#   
+#   outcome_list <- make_doce_outcome(combined_df_inc[1,],input_info)
+#   doce_pub <- make_doce_pub(input_info)
+#   
+#   single_trial <- make_trial(input_info,outcome_list,doce_pub,"nct111")
+#   write_trial(single_trial, trial_con_db)
+#   
+# })
+
+
 
