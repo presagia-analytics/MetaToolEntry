542355544d619ea1de7a9b1b2405a110668fd20c
Sophia Jia <xjia@presagia.ai>
2022-06-24 16:59:44 -0400

    Update module_cat.R

--- module/module_cat.R
+++ module/module_cat.R
@@ -73,31 +73,3 @@ server_cat <- function(id, app_values) {
     }
   )
 }
-
-# library(shiny)
-# library(rhandsontable)
-# source(file.path(here::here(), "utils/functions_helper.R"), encoding = 'UTF-8')
-# 
-# ui <- fluidPage(
-#   ui_cat ("counter1"),
-#   hr(),
-#     textOutput("text"),
-#     dataTableOutput('table')
-#   )
-# 
-# server <- function(input, output, session) {
-#   values <- reactiveValues()
-#   df <- read.csv(file.path(here::here(),"FirstDataSource.csv"))
-#   values[['summary_table']] <- df
-#   values[["all_outcome"]] <- NULL
-#   server_cat("counter1",values)
-#   output$text <- renderText({
-#     paste0(names(values),sep = "  //")
-#   })
-#   output$table <- renderDataTable(
-#     #browser()
-#     values[["all_outcome"]]
-#     )
-# }
-# 
-# shinyApp(ui, server)
